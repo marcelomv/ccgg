84b1403815fc9659acbadb34e03c4abdeb79cc0e
Marcelo Muñoz Linux Mint <marcelo@Araucano.(none)>
2012-11-17 16:47:57 -0300

    primer scrip de mi nuesvo sistema

---
+++ body.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+	<title>Control de Todo</title>
+</head>
+<body>
+<div>
+<p>Empresa :</p> <p>Usuario :</p>
+</div>
+
+<h2>... Control Total ...</h2>
+
+<p>
+
+<?php
+
+?>
+
+
+</p>
+
+</body>
